036cbab27787d87a5af8679162dc598a8e7fbfe2
cole0995 <cole0995@bu.edu>
2017-12-12 21:38:02 -0500

    Update simulator-action.js

--- js/simulator-action.js
+++ js/simulator-action.js
@@ -102,7 +102,7 @@ function sendSenderPacket() {
 	var seq = Math.floor(numTransmissions/2)
 	if (flag == "3_WAY_HANDSHAKE"
 	   && numTransmissions == 3) {
-		updatePacket({"SYN": 0, "SEQ":42 + seq, "ACK": 79 + seq, "DATA": String.fromCharCode('A'.charCodeAt(0) + seq)});
+		updatePacket({"SYN": 0, "SEQ":42 + seq + 1, "ACK": 79 + seq, "DATA": String.fromCharCode('A'.charCodeAt(0) + seq)});
 	}
 		
 	else {
@@ -112,13 +112,9 @@ function sendSenderPacket() {
 
 function sendReceiverPacket() {
 	var seq = Math.floor(numTransmissions/2)
-	if (flag == "3_WAY_HANDSHAKE"
-	   && numTransmissions == 3) {
-		updatePacket({"SYN": 0, "SEQ":42 + seq, "ACK": 79 + seq, "DATA": String.fromCharCode('A'.charCodeAt(0) + seq)});
-	}
-	else {	
+	//else {	
 		updatePacket({ "SEQ": 79 + seq, "ACK": 42 + seq + 1, "DATA": String.fromCharCode('Z'.charCodeAt(0) - seq) });
-	}
+	//}
 }
 
 // TODO do this better!
