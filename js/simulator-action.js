d18a0edd8a4386260bc3075735371853290887fb
cole0995 <cole0995@bu.edu>
2017-12-13 21:52:49 -0500

    Update simulator-action.js

--- js/simulator-action.js
+++ js/simulator-action.js
@@ -66,7 +66,7 @@ function packetMover(delta) {
 		
 		else if (flag == "CONNECTION_CLOSE"
 			&& numTransmissions == 2) {
-			numTransmissions = 4;
+			numTransmissions = 2;
 			direction = RECEIVER;
 			packets.y = height/2;
 			resetLine = 1;
